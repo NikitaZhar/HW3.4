c9ff2a44ac56236ab71832b350a961f279781226
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-04-06 19:20:26 +0200

    Initial Commit

---
+++ HW3.4/HW3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_4App.swift
+//  HW3.4
+//
+//  Created by Nikita Zharinov on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
